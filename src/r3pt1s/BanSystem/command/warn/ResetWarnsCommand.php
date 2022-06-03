ca1a00e318280d2fa53a35c99a57d0e7f47a9b6d
r3pt1s <54471820+r3pt1s@users.noreply.github.com>
2022-06-03 15:43:19 +0200

    Add files via upload

---
+++ src/r3pt1s/BanSystem/command/warn/ResetWarnsCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace r3pt1s\BanSystem\command\warn;
+
+use r3pt1s\BanSystem\BanSystem;
+use r3pt1s\BanSystem\manager\warn\WarnManager;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use pocketmine\player\Player;
+
+class ResetWarnsCommand extends Command {
+
+    public function __construct(string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = []) {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+        $this->setPermission("resetwarns.command");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if ($sender instanceof Player) {
+            if ($sender->hasPermission($this->getPermission())) {
+                if (isset($args[0])) {
+                    if (BanSystem::getInstance()->isPlayerCreated($args[0])) {
+                        $sender->sendMessage(BanSystem::getPrefix() . "§7The warns of the player §e" . $args[0] . " §7were removed!");
+                        WarnManager::getInstance()->removeWarns($args[0]);
+                    } else {
+                        $sender->sendMessage(BanSystem::getPrefix() . "§7The player §e" . $args[0] . " §7doesn't exists!");
+                    }
+                } else {
+                    $sender->sendMessage(BanSystem::getPrefix() . "§c/resetwarns <player>");
+                }
+            } else {
+                $sender->sendMessage(BanSystem::getPrefix() . BanSystem::NO_PERMS);
+            }
+        }
+        return true;
+    }
+}
---
+++ src/r3pt1s/BanSystem/form/warn/ViewWarnForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace r3pt1s\BanSystem\form\warn;
+
+use dktapps\pmforms\MenuForm;
+use dktapps\pmforms\MenuOption;
+use pocketmine\player\Player;
+
+class ViewWarnForm extends MenuForm {
+
+    private array $options = [];
+
+    public function __construct(array $warnData, string $target) {
+        $text = "§7Player: §e" . $target . "\n";
+        $text .= "§7Moderator: §e" . $warnData["Moderator"] . "\n";
+        $text .= "§7Warned At: §e" . $warnData["WarnedAt"] . "\n";
+        $text .= "§7Reason: §e" . $warnData["Reason"];
+
+        $this->options[] = new MenuOption("§cBack");
+
+        parent::__construct("§c" . $warnData["WarnedAt"], $text, $this->options, function (Player $player, int $data) use($target): void {
+            $player->sendForm(new WarnsForm($target));
+        });
+    }
+}
